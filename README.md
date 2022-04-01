051da1f709c207974469ceda705388182ebfef88
ggrove87 <gwg1387@gmail.com>
2022-04-01 13:29:34 -0500

    adding files

---
+++ README.md
@@ -0,0 +1,54 @@
+Contact: Gene Grove - gwg1387@gmail.com
+
+<br>
+
+Associated Links
+
+GIT Pages Link: https://ggrove87.github.io/HW3-Password-Generator/
+
+GIT Repository Link: https://github.com/ggrove87/HW3-Password-Generator
+
+<br>
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+<br>
+
+## Mock-Up
+
+The following image shows the web application's appearance and functionality:
+
+<br>
+Appearance Image:
+
+![Image of the webpage being deployed](./passwordGenerator.png)
+
+Functionality Image:
+
+![Image of the webpage fucntioning](./screenshotcompleteHW2.png)
--- script.js
+++ script.js
@@ -2,33 +2,32 @@
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-// Write password to the #password input
-// Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// loop it up! save yourself time
-
-// concatinate an arrays of what they select array.concat(array2)
+//Function to generate a password using the below defined arrays
 function generatePassword() {
   let lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   let numbersAvailable = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   let symbolsAvailable = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  //The return value of the function
   let finalPassword = "";
+  //A blank array which will be updated based on the user selected inputs from the prompts
   let userSelections = [""];
 
+  // Requests input from the user 
   let passwordLength = window.prompt("How many charactes would you like your password to be?");
   let passwordLowerCase = window.confirm("Would you like to use lower case letters?");
   let passwordUpperCase = window.confirm("Would you like to use upper case letters?");
   let passwordNumbers = window.confirm("Would you like to use numbers?");
   let passwordSplChars = window.confirm("Would you like to use special characters?");
-  
+
+// If the user does not select any of the possible requirements for the password they are prompted again.
 if (passwordLowerCase === false && passwordUpperCase === false && passwordNumbers === false && passwordSplChars === false) {
   do  {
     window.alert("Please make a selection");
@@ -38,9 +37,12 @@ if (passwordLowerCase === false && passwordUpperCase === false && passwordNumber
     passwordSplChars = window.confirm("Would you like to use special characters?");
   }  while (passwordLowerCase === false && passwordUpperCase === false && passwordNumbers === false && passwordSplChars === false);
 }
+// Checks if the password is between 8-128 characters
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Please enter a number of 8 or more and 128 or less.")
-  } else {
+  } 
+  //Adds the requested values based on user input to userSelection to create the pool of characters to be used for the password.
+  else {
     if (passwordLowerCase === true) {
       userSelections = userSelections.concat(lowerCaseLetters);
     }
@@ -53,7 +55,7 @@ if (passwordLowerCase === false && passwordUpperCase === false && passwordNumber
     if (passwordSplChars === true) {
       userSelections = userSelections.concat(symbolsAvailable);
     }
-
+    //Runs a for look to create a password with length based on the user input and using the pool of charactered based on the user selections from the prompts.
     for (let i = 0; i < passwordLength; i++) {
       let rgn = Math.floor(Math.random() * userSelections.length);
       finalPassword = finalPassword + userSelections[rgn];
@@ -64,9 +66,3 @@ if (passwordLowerCase === false && passwordUpperCase === false && passwordNumber
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// let RPSLetter = ["R", "P", "S"];
-// let rgn = Math.floor(Math.random() * RPSLetter.length);
-// let rgnLetter = RPSLetter[rgn];
